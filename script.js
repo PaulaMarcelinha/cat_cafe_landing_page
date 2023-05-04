27aefc283a2142036dfe283be034858a2312a5d5
PaulaMarcelinha <98559181+PaulaMarcelinha@users.noreply.github.com>
2023-05-04 23:15:41 +0100

    added new section -cat quote generator, basic, need to work on css and tidy it up

--- script.js
+++ script.js
@@ -11,3 +11,26 @@ function toggleModal() {
 
 openButton.addEventListener('click', toggleModal);
 closeButton.addEventListener('click', toggleModal);
+
+
+// CAT QUOTE GENERATOR
+
+let quotes = [
+    "\“All you need is love and a cat.\” – Unknown",
+    "\“In ancient times cats were worshipped as gods; they have not forgotten this.\” – Terry Pratchett",
+    "\“Time spent with cats is never wasted.\” – Sigmund Freud",
+    "\“Cats choose us; we don’t own them.\” – Kristin Cast",
+    "\"Meow meeeeooooow\" -Mei"
+    
+    
+  ];
+  
+  
+  function newQuote() {
+    let randomNumber = Math.floor(Math.random() * (quotes.length));
+    document.getElementById('catQuote').innerHTML = quotes[randomNumber];
+    }
+  
+  window.onload=newQuote;  //when page loads, quote appears
+  
+  
